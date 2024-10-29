c1fa3f9558a2626c01e5f4ac999663af4a67bbe2
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2024-10-29 20:31:23 +0700

    add uploadCloud.middleware.js

---
+++ middlewares/admin/uploadCloud.middleware.js
@@ -0,0 +1,45 @@
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
+// Connect cloudinary
+cloudinary.config({
+  cloud_name: 'dlnpm0kdo',
+  api_key: '258652766361362',
+  api_secret: 'Wk_vFnGpZat8d7Y2DUSMcObBIOc'
+});
+// End cloudinary
+
+module.exports.upload = async (req, res, next) => {
+  if (req.files && req.files.length > 0) {
+    let streamUpload = (file) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream(
+          (error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          }
+        );
+
+        streamifier.createReadStream(file.buffer).pipe(stream);
+      });
+    };
+
+    try {
+      const uploadPromises = req.files.map(file => streamUpload(file));
+
+      const results = await Promise.all(uploadPromises);
+
+      req.body.files = results.map(result => result.secure_url);
+
+      next();
+    } catch (error) {
+      console.error("Upload error:", error);
+      res.status(500).send("Error uploading files");
+    }
+  } else {
+    next();
+  }
+}
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+const upload = multer();
+const uploadCloud = require('../../middlewares/admin/uploadCloud.middleware');
+
 const controller = require('../../controllers/admin/product.controller');
 
 router.get('/', controller.index);
