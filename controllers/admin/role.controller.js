3b03ff2ed348bc37871e2f9d0df5c5507a13a0d5
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2024-11-03 23:02:17 +0700

    update CRUD role.controller.js

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -15,3 +15,68 @@ module.exports.index = async(req, res) => {
     roles: roles,
   })
 }
+
+// [PATCH] /admin/roles/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+  const id = req.params.id;
+  const status = req.params.status;
+
+  await Role.updateOne({ _id: id }, { status: status });
+
+  res.redirect(`${prefixAdmin}/roles`);
+}
+
+// [DELETE] /admin/roles/delete/:id
+module.exports.delete = async (req, res) => {
+  const id = req.params.id;
+
+  await Role.updateOne({ _id: id }, { deleted: true });
+
+  res.redirect(`${prefixAdmin}/roles`);
+}
+
+// [GET] /admin/roles/create
+module.exports.create = (req, res) => {
+  res.render('admin/roles/create', {
+    pageTitle: 'Create Role',
+    currentPage: 'roles',
+  });
+}
+
+// [POST] /admin/roles/create
+module.exports.createPost = async(req, res) => {
+  const { name, description, status } = req.body;
+  const data = { name, description, status };
+
+  const role = new Role(data);
+  await role.save();
+
+  res.redirect(`${prefixAdmin}/roles`);
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const role = await Role.findOne({ _id: id }).lean();
+
+    res.render('admin/roles/edit', {
+      pageTitle: 'Update Role',
+      currentPage: 'roles',
+      role: role,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPost = async(req, res) => {
+  const id = req.params.id;
+  const { name, description, status } = req.body;
+  const data = { name, description, status };
+
+  await Role.updateOne({ _id: id }, data);
+
+  res.redirect(`${prefixAdmin}/roles`);
+}
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -5,4 +5,16 @@ const controller = require('../../controllers/admin/role.controller');
 
 router.get('/', controller.index);
 
+router.patch('/change-status/:status/:id', controller.changeStatus);
+
+router.delete('/delete/:id', controller.delete);
+
+router.get('/create', controller.create);
+router.post('/create', controller.createPost);
+
+router.get('/edit/:id', controller.edit);
+router.patch('/edit/:id', controller.editPost);
+
+
+
 module.exports = router;
