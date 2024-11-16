a57072a5d6be6260ccb81ddeb4b2f65883a6888f
KhanhHuynh <khanh@duck.com>
2024-11-16 22:20:33 +0700

    feat: add product detail page

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,7 @@ const shopRouter = require('./shop.route');
 const authRoutes = require('./auth.route');
 const userRoutes = require('./user.route');
 const contactRoutes = require('./contact.route');
+const productRoutes = require('./product.route');
 
 
 const authMiddleware = require('../../middlewares/client/auth.middleware');
@@ -17,5 +18,6 @@ module.exports = (app) => {
     app.use('/shop', shopRouter);
     app.use('/user', userRoutes);
     app.use('/contact', contactRoutes);
+    app.use('/product', productRoutes);
 
 }
